051eb33e4ef8edd7044214bf80ed21da0e7acad4
Jamie Thomson <thomson.j@etoncollege.org.uk>
2022-01-20 11:48:48 +0000

    Initial Commit

---
+++ TamagotchiApp/TamagotchiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppApp.swift
+//  TamagotchiApp
+//
+//  Created by Thomson, Jamie (NA) on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
